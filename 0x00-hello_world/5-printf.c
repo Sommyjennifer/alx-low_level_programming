ce1f55d2e309932af97e42a31f5a92dc27fdb45e
Sommyjennifer <nnamdijennifer@gmail.com>
2022-09-23 18:09:36 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - Prints a text with proper grammar
  *
- * Return: Always 0 (Successful)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art, \n");
-	return (0);
+printf("\"with proper grammar, but the outcome is a piece of art, \n");
+return (0);
 }
